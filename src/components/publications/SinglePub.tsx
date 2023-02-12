a0c58279a262faf688371258f1853796aeb273d9
fabri2605 <fabridp77@gmail.com>
2023-02-12 15:59:13 -0300

    fix

--- src/components/publications/SinglePub.tsx
+++ src/components/publications/SinglePub.tsx
@@ -2,7 +2,7 @@ import {
     publication,
     PublicationsContext,
 } from '../../context/PublicationsContext';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
